4d2eedf233fdd5736f06b2a0b630978162dc0477
Christopher Dimoulis <christopherjamesd@gmail.com>
2018-04-29 13:58:47 -0500

    Moved routes off clearance.

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -2,6 +2,6 @@ Clearance.configure do |config|
   config.mailer_sender = "donotreply@oursiders.com"
   config.cookie_domain = 'oursiders.com'
   config.rotate_csrf_on_sign_in = true
-  config.routes = true
+  config.routes = false
   config.sign_in_guards = [Auth::LockedGuard];
 end
