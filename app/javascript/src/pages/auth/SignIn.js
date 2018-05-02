b8d3074c9698d898fa207ff14f3b3ebdd05190f7
Christopher Dimoulis <christopherjamesd@gmail.com>
2018-05-02 17:22:05 -0500

    Transitions working

--- app/javascript/src/pages/auth/SignIn.js
+++ app/javascript/src/pages/auth/SignIn.js
@@ -1,36 +1,50 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Form from "./components/form"
+import { CSSTransition } from "react-transition-group"
 
 class SignIn extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      show: false,
+    }
+  }
+
+  componentDidMount() {
+    this.setState({show: true});
+  }
+
   render () {
     return (
       <div className='container'>
         <div className='row'>
           <div className='col s6 offset-s3'>
-            <div className='card-panel'>
-              <div className='header text-center'>
-                <img className='sign-in-logo' src='assets/logo_250.webp'>
-                </img>
-                <h3>
-                  Sign In
-                </h3>
-                <div>
-                  No Account?
-                  <a href={this.props.signupUrl}>  Sign Up</a>
+            <CSSTransition in={this.state.show} timeout={100} classNames="auth">
+              <div className='card-panel'>
+                <div className='header text-center'>
+                  <img className='sign-in-logo' src='assets/logo_250.webp'>
+                  </img>
+                  <h3>
+                    Sign In
+                  </h3>
+                  <div>
+                    No Account?
+                    <a href={this.props.signupUrl}>  Sign Up</a>
+                  </div>
+                </div>
+                <div className='row'>
+                  <div className='col s12'>
+                    <Form url={this.props.submitUrl} buttonText='Sign In' type='session'></Form>
+                  </div>
+                </div>
+                <div className='forgot text-right'>
+                  <a href='#'>
+                    Forgot your password?
+                  </a>
                 </div>
               </div>
-              <div className='row'>
-                <div className='col s12'>
-                  <Form url={this.props.submitUrl} buttonText='Sign In' type='session'></Form>
-                </div>
-              </div>
-              <div className='forgot text-right'>
-                <a href='#'>
-                  Forgot your password?
-                </a>
-              </div>
-            </div>
+            </CSSTransition>
           </div>
         </div>
       </div>
--- app/javascript/src/pages/auth/SignUp.js
+++ app/javascript/src/pages/auth/SignUp.js
@@ -1,27 +1,41 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Form from "./components/form"
+import { CSSTransition } from "react-transition-group"
 
 class SignUp extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      show: false,
+    }
+  }
+
+  componentDidMount() {
+    this.setState({show: true});
+  }
+
   render () {
     return (
       <div className='container'>
         <div className='row'>
           <div className='col s6 offset-s3'>
-            <div className='card-panel'>
-              <div className='header text-center'>
-                <img className='sign-in-logo' src='assets/logo_250.webp'>
-                </img>
-                <h3 className='text-center'>
-                  Sign Up
-                </h3>
-                <div>
-                  Already have an account?
-                  <a href={this.props.signinUrl}>  Sign In</a>
+            <CSSTransition in={this.state.show} timeout={100} classNames="auth">
+              <div className='card-panel'>
+                <div className='header text-center'>
+                  <img className='sign-in-logo' src='assets/logo_250.webp'>
+                  </img>
+                  <h3 className='text-center'>
+                    Sign Up
+                  </h3>
+                  <div>
+                    Already have an account?
+                    <a href={this.props.signinUrl}>  Sign In</a>
+                  </div>
                 </div>
+                <Form url={this.props.submitUrl} buttonText='Sign Up' type='user'></Form>
               </div>
-              <Form url={this.props.submitUrl} buttonText='Sign Up' type='user'></Form>
-            </div>
+            </CSSTransition>
           </div>
         </div>
       </div>
