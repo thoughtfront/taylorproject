c80fdc6cc725fd7fb27dd082aca372baaa448c96
Christopher Dimoulis <christopherjamesd@gmail.com>
2018-05-14 10:49:27 -0500

    Example to components

--- app/javascript/src/pages/home/index.js
+++ app/javascript/src/pages/home/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import Example from '../../dev/example'
+import Example from '../../components/example'
 
 class Main extends React.Component {
   render() {
